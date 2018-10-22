ed54857c6ba81dd8b78a2486604e2f9bb1bce5d1
Yelyzaveta L <36056528+ElizaLo@users.noreply.github.com>
2018-10-22 22:21:22 +0300

    Create CHOOSE2.cpp

---
+++ Interview Bit/Time Complexity/CHOOSE2.cpp
@@ -0,0 +1,14 @@
+Which of the given options provides the increasing order of complexity of functions f1, f2, f3 and f4:
+
+
+f1(n) = 2^n
+
+f2(n) = n^(3/2)
+
+f3(n) = nLogn
+
+f4(n) = n^(Logn)
+
+
+ANSWER:
+f3, f2, f4, f1
