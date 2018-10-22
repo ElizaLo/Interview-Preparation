62d0e0f076fb74667ff0dcc0dd7ca7f1abb2a6e2
Yelyzaveta L <36056528+ElizaLo@users.noreply.github.com>
2018-10-22 21:49:29 +0300

    Create NESTED_CMPL3.cpp

---
+++ Interview Bit/Time Complexity/NESTED_CMPL3.cpp
@@ -0,0 +1,10 @@
+// What is time complexity of following code :
+
+        int count = 0;
+        for (int i = N; i > 0; i /= 2) {
+            for (int j = 0; j < i; j++) {
+                count += 1;
+            }
+        }
+        
+//  O(N)
