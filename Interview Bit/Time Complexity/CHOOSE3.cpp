d68a80a5c0ef6eeef643b85c8de9a0feb2d9996a
Yelyzaveta L <36056528+ElizaLo@users.noreply.github.com>
2018-10-22 18:47:53 +0300

    Create CHOOSE3.cpp

---
+++ Interview Bit/Time Complexity/CHOOSE3.cpp
@@ -0,0 +1,20 @@
+In a competition, four different functions are observed. All the functions use a single for loop and within the for loop, same set of statements are executed.
+
+Consider the following for loops:
+
+
+  A) for(i = 0; i < n; i++)
+ 
+  B) for(i = 0; i < n; i += 2)
+ 
+  C) for(i = 1; i < n; i *= 2)
+ 
+  D) for(i = n; i > -1; i /= 2)
+  
+  
+If n is the size of input(positive), which function is the most efficient? In other words, which loop completes the fastest.
+
+
+
+ANSWER:
+N
